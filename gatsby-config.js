72d858a248c2906795e4be261f2e45ba9102bff2
Chadwin <chadwinjdeysel@gmail.com>
2021-02-23 18:10:59 +0200

    removed empty gatsby-source-filesystem directories

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,22 +15,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
         path: `${__dirname}/src/pages/`,
         name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
       },
     },
     {
